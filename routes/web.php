0d401cc3ff71fe2761169705146d5d193cd9e078
nicolasjveiga <nickjveiga@gmail.com>
2025-06-20 10:06:17 -0300

    Firt Commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\{SupportController};
+
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+
+Route::get('/', function () {
+    return view('welcome');
+});
