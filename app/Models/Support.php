af8e9b2bdeb63c066884671ea9ffeed06f6d0939
nicolasjveiga <nickjveiga@gmail.com>
2025-06-20 11:21:22 -0300

    Store supports

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Support extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'subject',
+        'status',
+        'body',
+    ];
 }
