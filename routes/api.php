9e8632ebb5e1444569f3b2d1004a3628a31f0988
nicolasjveiga <nickjveiga@gmail.com>
2025-06-25 09:29:48 -0300

    Adicionando Factories, controllers e rotas para produtos e categorias

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/supports', [SupportController::class, 'indexJson']);
+
+Route::get('/products', [ProductController::class, 'index']);
